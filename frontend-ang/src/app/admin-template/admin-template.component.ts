75192221abadd9ab1cd78f8189eb5a21a50868ea
Sikati Kenmogne Samuel <71256717+sikatikenmogne@users.noreply.github.com>
2024-04-28 13:35:01 +0100

    Create admin page component - Add header

---
+++ frontend-ang/src/app/admin-template/admin-template.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-template',
+  standalone: true,
+  imports: [],
+  templateUrl: './admin-template.component.html',
+  styleUrl: './admin-template.component.css'
+})
+export class AdminTemplateComponent {
+
+}
--- frontend-ang/src/app/app.module.ts
+++ frontend-ang/src/app/app.module.ts
@@ -4,6 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbar} from "@angular/material/toolbar";
+import {MatButton, MatIconButton} from "@angular/material/button";
+import {MatChipTrailingIcon} from "@angular/material/chips";
+import {MatIcon, MatIconModule} from "@angular/material/icon";
+import {MatMenu, MatMenuModule} from "@angular/material/menu";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatList, MatListItem} from "@angular/material/list";
 
 @NgModule({
   declarations: [
@@ -12,7 +19,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbar,
+    MatButton,
+    MatIconModule,
+    MatChipTrailingIcon,
+    MatIcon,
+    MatIconButton,
+    MatMenuModule,
+    MatSidenavModule,
+    MatList,
+    MatListItem
   ],
   providers: [],
   bootstrap: [AppComponent]
